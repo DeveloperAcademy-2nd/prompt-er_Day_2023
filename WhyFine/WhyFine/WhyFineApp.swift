9406106d59267d8c3eab5073a6d68ce5094a1e6a
eratchacha <papa021326@naver.com>
2023-09-01 17:01:10 +0900

    Initial Commit

---
+++ WhyFine/WhyFine/WhyFineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyFineApp.swift
+//  WhyFine
+//
+//  Created by 차차 on 2023/09/01.
+//
+
+import SwiftUI
+
+@main
+struct WhyFineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
